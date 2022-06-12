592f839c6d47a00e0d75473a9b0895a04d6530b1
ramiporta <p0rt4csd@gmail.com>
2022-06-12 03:30:09 -0300

    typewriter

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+//Activar y desactivar "tilt" segun tamaño de pantalla.
 const tiltScriptSrc = "./assets/js/vanilla-tilt.min.js";
 function addScript(src) {
     const script = document.createElement("script");
@@ -14,3 +15,21 @@ if (!isMobile()){
     const hexagons = document.querySelectorAll(".hexagon__list > div");
     hexagons.forEach(element => element.classList.add("tilt"));
 }
+
+//Efecto maquina de escribir (typewriter) en titulo.
+const title = document.querySelector(".typewriter");
+const writing = element =>{
+    let i = 0;
+    let str = element.textContent;
+    const arrStr = str.split("");
+    element.textContent = "";
+    const printStr = setInterval(()=>{
+        element.textContent += arrStr[i];
+        i++;
+        if (i == str.length){
+            setTimeout(writing, 5000, element);
+            clearInterval(printStr);
+        };
+    },80)
+}
+writing(title);
